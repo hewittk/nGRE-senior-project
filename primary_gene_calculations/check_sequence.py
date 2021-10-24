16d5e8aadc7a2678baa31fe1eab637d07228c23e
Kiley <42876297+hewittk@users.noreply.github.com>
2021-10-24 16:48:39 -0400

    Organized folder

--- primary_gene_calculations/check_sequence.py
+++
@@ -1,6 +0,0 @@
-# get start and end for gene and add/subtract appropriate value
-# ping ucsc api to get and store sequence
-# basic analyses:
-    # time and run regex analysis on sequence
-    # time and run Biopython analysis on sequence
-    # return and save scores and nucleotide numbers/sequences of top potential sites
