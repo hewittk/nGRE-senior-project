b6c92e9c270aa3978869368c9f9169b963efe971
Kiley <42876297+hewittk@users.noreply.github.com>
2021-10-27 19:47:59 -0400

    Requests method

--- api_seq_request.py
+++ api_seq_request.py
@@ -3,13 +3,17 @@ import pandas as pd
 
 genes_df = pd.read_csv("annotated_gene_datasets/ADX_F_DexvsADX_F_Veh/downregulated_genes/downregulated_output_with_gene_names.tsv", sep = "\t")
 
-def geneCoordinates(dataframe):
+def geneCoordinates(column):
     print("In geneCoordinates")
-    chromosome = dataframe["mm10.knownGene.chrom"][1]
-    transcriptionStart = dataframe["mm10.knownGene.txStart"][1]
-    transcriptionEnd = dataframe["mm10.knownGene.txEnd"][1]
+    chromosome = genes_df["mm10.knownGene.chrom"][column]
+    transcriptionStart = genes_df["mm10.knownGene.txStart"][column]
+    transcriptionEnd = genes_df["mm10.knownGene.txEnd"][column]
 
     return chromosome, transcriptionStart, transcriptionEnd
+
+def request(chromosome, startCoordinate, endCoordinate):
+    response = requests.get("http://api.genome.ucsc.edu/getData/sequence?genome=mm10;chrom=" + str(chromosome) + ";start=" + str(startCoordinate) + ";end=" + str(endCoordinate))
+    print(response.status_code)
 
 def method():
     # get gene name and chromosome, txStart, txEnd for each gene
@@ -17,8 +21,7 @@ def method():
 
     # request sequence for gene
     response = requests.get("http://api.genome.ucsc.edu/getData/sequence?genome=mm10;chrom=chr1;start=20542952;end=20618064")
-    print(response.status_code)
-    print(response.json())
+    # print(response.json())
 
     # mine json for response
 
@@ -27,8 +30,9 @@ def method():
 
 def main():
     print("In main")
-    print(geneCoordinates(genes_df))
-
+    for i in range(len(genes_df)):
+        chromosome, startCoordinate, endCoordinate = geneCoordinates(i)
+        request(chromosome, startCoordinate, endCoordinate)
 
 if __name__ == "__main__":
     main()
