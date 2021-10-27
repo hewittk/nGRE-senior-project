66674b5153f884540712eeb63f75d98f5c7b41a0
Kiley <42876297+hewittk@users.noreply.github.com>
2021-10-27 18:25:52 -0400

    Get gene coordinates method

--- api_seq_request.py
+++ api_seq_request.py
@@ -1,20 +1,34 @@
 import requests
+import pandas as pd
 
-# get gene name and chromosome, txStart, txEnd for each gene
+genes_df = pd.read_csv("annotated_gene_datasets/ADX_F_DexvsADX_F_Veh/downregulated_genes/downregulated_output_with_gene_names.tsv", sep = "\t")
+
+def geneCoordinates(dataframe):
+    print("In geneCoordinates")
+    chromosome = dataframe["mm10.knownGene.chrom"][1]
+    transcriptionStart = dataframe["mm10.knownGene.txStart"][1]
+    transcriptionEnd = dataframe["mm10.knownGene.txEnd"][1]
+
+    return chromosome, transcriptionStart, transcriptionEnd
+
+def method():
+    # get gene name and chromosome, txStart, txEnd for each gene
 
 
-# request sequence for gene
-response = requests.get("http://api.genome.ucsc.edu/getData/sequence?genome=mm10;chrom=chr1;start=20542952;end=20618064")
-print(response.status_code)
-print(response.json())
+    # request sequence for gene
+    response = requests.get("http://api.genome.ucsc.edu/getData/sequence?genome=mm10;chrom=chr1;start=20542952;end=20618064")
+    print(response.status_code)
+    print(response.json())
 
-# mine json for response
+    # mine json for response
 
-# save to file named after gene
+    # save to file named after gene
 
 
 def main():
     print("In main")
+    print(geneCoordinates(genes_df))
+
 
 if __name__ == "__main__":
     main()
