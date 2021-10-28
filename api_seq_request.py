df2024c29cd2710aeb9819e3f99b82780adb9292
Kiley <42876297+hewittk@users.noreply.github.com>
2021-10-27 22:34:30 -0400

    Capacity to send gene sequence to file

--- api_seq_request.py
+++ api_seq_request.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import json
 import pandas as pd
 
 genes_df = pd.read_csv("annotated_gene_datasets/ADX_F_DexvsADX_F_Veh/downregulated_genes/downregulated_output_with_gene_names.tsv", sep = "\t")
@@ -13,26 +15,37 @@ def geneCoordinates(column):
 
 def request(chromosome, startCoordinate, endCoordinate):
     response = requests.get("http://api.genome.ucsc.edu/getData/sequence?genome=mm10;chrom=" + str(chromosome) + ";start=" + str(startCoordinate) + ";end=" + str(endCoordinate))
-    print(response.status_code)
 
-def method():
-    # get gene name and chromosome, txStart, txEnd for each gene
+    json_sequence = response.json()
+    print(json_sequence)
+    # print(json_sequence['dna'])
 
+    return json_sequence['dna']
+    # print(response.status_code)
 
-    # request sequence for gene
-    response = requests.get("http://api.genome.ucsc.edu/getData/sequence?genome=mm10;chrom=chr1;start=20542952;end=20618064")
-    # print(response.json())
-
-    # mine json for response
-
+def write_to_file(gene_name, string):
     # save to file named after gene
+    os.mkdir("gene_sequences/" + gene_name)
+    file_name = gene_name + ".txt"
+
+    f = open("gene_sequences/" + gene_name + "/" + file_name, "x")
+    f.close()
+
+    with open("gene_sequences/" + gene_name + "/" + file_name, "w") as file:
+        file.write(string)
+        file.close()
 
 
 def main():
     print("In main")
+    """
     for i in range(len(genes_df)):
-        chromosome, startCoordinate, endCoordinate = geneCoordinates(i)
-        request(chromosome, startCoordinate, endCoordinate)
+        # chromosome, startCoordinate, endCoordinate = geneCoordinates(i)
+        # json_response = request(chromosome, startCoordinate, endCoordinate)
+    """
+    chromosome, startCoordinate, endCoordinate = geneCoordinates(0)
+    json_response = request(chromosome, startCoordinate, endCoordinate)
+    write_to_file("Genee", json_response)
 
 if __name__ == "__main__":
     main()
