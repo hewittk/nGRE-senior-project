aad6b01f98d1ecd4020cae051057f18eece80292
Kiley Hewitt <42876297+hewittk@users.noreply.github.com>
2021-12-12 06:26:17 -0500

    Store information on errors

--- gene_nGRE_parse.py
+++ gene_nGRE_parse.py
@@ -9,7 +9,7 @@ nGRE_consensus_sequence = "CTCCGGAGA"
 
 def localAlignment(gene_sequence, gene_id):
 	alignment_start = time.time()
-	# local alignment with match score of 3, mismatch penalty of -1.5, gap opening penalty of -.5, gap extension penalty of -5
+	# local alignment with match score of 3, mismatch penalty of -1.5, gap opening penalty of -.2, gap extension penalty of -.2
 	myAlignments = pairwise2.align.localms(gene_sequence, nGRE_consensus_sequence, 3, -1, -.2, -.2)
 
 	with open("nGRE_sample_parse_output/parse_" + gene_id + ".txt", "a") as file:
@@ -30,8 +30,17 @@ def regexSearch(gene_sequence, gene_id):
 
 	print(possible_matches)
 
+	# obtain information about location and number of errors in each nGRE
 	for nGRE_sequence in possible_matches:
-		print(regex.search(nGRE_sequence, gene_sequence))
+		# obtain potential nGRE sequence's location in gene sequence
+		pos_information = regex.search(nGRE_sequence, gene_sequence)
+		print(pos_information)
+
+		# compare to perfect nGRE for error counts
+		comparison = (regex.search(r"([Cc][Tt][Cc][Cc][TAGCtagc]?[TAGCtagc]?[TAGCtagc]?[Gg][Gg][Aa][Gg][Aa]){e<=3}", nGRE_sequence))
+		print(comparison.fuzzy_counts)
+
+	print()
 	# starts = possible_matches.starts()
 	# print(starts)
 	# account for errors
@@ -48,7 +57,7 @@ def main():
 	gene_id = "Ccnd2_ENSMUST00000201637.1"
 	interest_gene = file.read()
 
-	localAlignment(interest_gene, gene_id)
+	# localAlignment(interest_gene, gene_id)
 	regexSearch(interest_gene, gene_id)
 
 
