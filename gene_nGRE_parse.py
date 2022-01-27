283261a2644dea76f8a9329ef19e36da58060c41
Kiley Hewitt <42876297+hewittk@users.noreply.github.com>
2022-01-27 17:45:49 -0500

    Method to parse CSVs

--- gene_nGRE_parse.py
+++ gene_nGRE_parse.py
@@ -4,6 +4,9 @@ import Bio
 from Bio import pairwise2
 from Bio.Seq import Seq
 
+import pandas as pd
+
+
 
 nGRE_consensus_sequence = "CTCCGGAGA"
 
@@ -30,7 +33,7 @@ def regexSearch(gene_sequence, gene_id):
 	# find matches
 	possible_matches = regex.findall("([Cc][Tt][Cc][Cc][TAGCtagc]?[TAGCtagc]?[TAGCtagc]?[Gg][Gg][Aa][Gg][Aa]){e<=3}", gene_sequence)
 
-	print(possible_matches)
+	# print(possible_matches)
 
 	# obtain information about location and number of errors in each nGRE
 	for nGRE_sequence in possible_matches:
@@ -38,35 +41,44 @@ def regexSearch(gene_sequence, gene_id):
 		# obtain potential nGRE sequence's location in gene sequence
 		pos_information = regex.search(nGRE_sequence, gene_sequence)
 		print(pos_information)
-		# print(regex.findall(r"\(\d+,", str(pos_information)))
 
 		# retrieve and store start/end coordinates of nGRE
 		start_coordinate_regex = regex.findall(r"\(\d+,", str(pos_information))
 		start_coordinate_element = start_coordinate_regex[0]
-		print("start coordinate: ", start_coordinate_element[1:6])
+		# print("start coordinate: ", start_coordinate_element[1:6])
 
 		end_coordinate_regex = regex.findall(r" \d+\)", str(pos_information))
 		end_coordinate_element = end_coordinate_regex[0]
-		print("end coordinate: ", end_coordinate_element[1:6])
+		# print("end coordinate: ", end_coordinate_element[1:6])
 
 		# compare to perfect nGRE for error counts
 		comparison = (regex.search(r"([Cc][Tt][Cc][Cc][TAGCtagc]?[TAGCtagc]?[TAGCtagc]?[Gg][Gg][Aa][Gg][Aa]){e<=3}", nGRE_sequence))
-		print(comparison.fuzzy_counts)
+		# print(comparison.fuzzy_counts)
 		mutation_counts = comparison.fuzzy_counts
 		mismatch_count = mutation_counts[0]
 		insertion_count = mutation_counts[1]
 		deletion_count = mutation_counts[2]
-		print(mismatch_count, insertion_count, deletion_count)
+		# print(mismatch_count, insertion_count, deletion_count)
 
-		print()
-	# starts = possible_matches.starts()
-	# print(starts)
-	# account for errors
+		# print()
 
 	regex_end = time.time()
 	regex_runtime = regex_end - regex_start
-	print("Local alignment finished, total runtime: {} seconds".format(regex_runtime))
+	print("Regex search completed, total runtime: {} seconds".format(regex_runtime))
 
+def csv_parse(treatment, regulation):
+	genes_df = pd.read_csv("annotated_gene_datasets/" + treatment + "/" + regulation + "_genes/" + regulation + "_output_with_gene_names.tsv", sep = "\t")
+	print("Test")
+
+	for column in range(len(genes_df)):
+		print(column)
+		gene_id = genes_df["mm10.kgXref.geneSymbol"][column] + "_" + genes_df["#mm10.knownGene.name"][column]
+		print(gene_id)
+
+		file = open("gene_sequences/" + gene_id + "/" + gene_id + ".txt")
+		interest_gene_sequence = file.read()
+
+		regexSearch(interest_gene_sequence, gene_id)
 
 def main():
 	start = time.time()
@@ -78,6 +90,7 @@ def main():
 	# localAlignment(interest_gene, gene_id)
 	regexSearch(interest_gene, gene_id)
 
+	csv_parse("ADX_F_DexvsADX_F_Veh", "upregulated")
 
 	end = time.time()
 	runtime = end - start
