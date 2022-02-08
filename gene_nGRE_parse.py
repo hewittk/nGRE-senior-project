9b9a719726141d13b18ba914b57710362313fff7
Kiley Hewitt <42876297+hewittk@users.noreply.github.com>
2022-02-07 20:45:45 -0500

    nGRE parse of OVX_ADX_F_DexvsOVX_ADX_F_Veh downregulated genes

--- gene_nGRE_parse.py
+++ gene_nGRE_parse.py
@@ -90,7 +90,7 @@ def csv_parse(treatment, regulation):
 def main():
 
 	start = time.time()
-	csv_parse("OVX_ADX_F_DexvsOVX_ADX_F_Veh", "upregulated")
+	csv_parse("OVX_ADX_F_DexvsOVX_ADX_F_Veh", "downregulated")
 	end = time.time()
 
 	runtime = end - start
