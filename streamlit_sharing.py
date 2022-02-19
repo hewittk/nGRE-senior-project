3878681f204a2efbb45e351967705905bbded49d
Kiley Hewitt <42876297+hewittk@users.noreply.github.com>
2022-02-19 16:56:49 -0500

    Selection of maximum number of mutations

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -6,5 +6,11 @@ st.write("My First Streamlit Web App")
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 
+maximum_mutations = st.selectbox(
+     'What is the maximum number of mutations in the nGRE consensus sequence that you want to tolerate?',
+     ('0', '1', '2', '3'))
+print("Maximum mutations in potential nGREs: ", maximum_mutations)
+
+
 user_input = st.text_area("Insert DNA sequence to parse for nGREs")
 user_input
