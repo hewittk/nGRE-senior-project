a5429441e3af8e5ce5a7d4284e8bc90084ff1609
Kiley Hewitt <42876297+hewittk@users.noreply.github.com>
2022-03-02 21:51:05 -0500

    OVX_ADX_F_DexvsOVX_ADX_F_Veh upregulated genes with mutation bug fixed

--- gene_nGRE_parse.py
+++ gene_nGRE_parse.py
@@ -89,7 +89,7 @@ def csv_parse(treatment, regulation):
 def main():
 
 	start = time.time()
-	csv_parse("OVX_ADX_F_DexvsOVX_ADX_F_Veh", "downregulated")
+	csv_parse("OVX_ADX_F_DexvsOVX_ADX_F_Veh", "upregulated")
 	end = time.time()
 
 	runtime = end - start
