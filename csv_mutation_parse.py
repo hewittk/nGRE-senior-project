644d6dd9eafca2e2ba71c7ca3fb3b5a2623bacdc
Kiley Hewitt <42876297+hewittk@users.noreply.github.com>
2022-03-03 19:57:46 -0500

    Redo of 0-1 mutations parse on new files and with summary file

--- csv_mutation_parse.py
+++ csv_mutation_parse.py
@@ -13,12 +13,19 @@ def fewer_mutations_parse(treatment, regulation):
     zero_mutation_subsequences = 0
     one_mutation_subsequences = 0
 
+    # dataset containing potential nGREs with fewer mutations
     fewer_mutations_table = pd.DataFrame(columns = ["gene_id", "chromosome", "nGRE_sequence", "start_site", "end_site", "mutations", "mismatch_mutations", "insertion_mutations", "deletion_mutations"])
 
-    zero_mutations = df.loc[df['mutations'] == 0]
-    fewer_mutations_table = fewer_mutations_table.append(zero_mutations, ignore_index = True)
-    one_mutation = df.loc[df['mutations'] == 1]
-    fewer_mutations_table = fewer_mutations_table.append(one_mutation, ignore_index = True)
+    with open('nGRE_parse_output/fewer_mutations_parse_statistics.txt', 'a') as output_file:
+        print("In the " + treatment + " " + regulation + " dataset (length " + str(len(df)) + " genes):", file = output_file)
+        zero_mutations = df.loc[df['mutations'] == 0]
+        fewer_mutations_table = fewer_mutations_table.append(zero_mutations, ignore_index = True)
+        print("Number of potential nGREs with zero mutations found: ", len(zero_mutations), file = output_file)
+
+        one_mutation = df.loc[df['mutations'] == 1]
+        fewer_mutations_table = fewer_mutations_table.append(one_mutation, ignore_index = True)
+        print("Number of potential nGREs with one mutation found: ", len(one_mutation), file = output_file)
+        print(" ", file = output_file)
 
     print(fewer_mutations_table)
 
@@ -27,7 +34,7 @@ def fewer_mutations_parse(treatment, regulation):
 def main():
 
 	start = time.time()
-	fewer_mutations_parse("ADX_F_DexvsADX_F_Veh", "upregulated", 1)
+	fewer_mutations_parse("OVX_ADX_F_DexvsOVX_ADX_F_Veh", "downregulated")
 	end = time.time()
 
 	runtime = end - start
