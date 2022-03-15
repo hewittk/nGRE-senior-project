bde6bd07a5eab08e8c75ee05939203cc3ccefed4
Kiley Hewitt <42876297+hewittk@users.noreply.github.com>
2022-03-15 00:24:16 -0400

    User input of mismatch/insertion/deletion penalties

--- streamlit/nGRE_parse.py
+++ streamlit/nGRE_parse.py
@@ -7,13 +7,25 @@ def main():
     global maximum_mutations
     st.write("Parse gene sequences for nGREs")
 
+    gene_sequence = st.text_area("Insert DNA sequence to parse for nGREs")
+    st.write(gene_sequence)
+
     maximum_mutations = st.selectbox(
          'What is the maximum number of mutations in the nGRE consensus sequence that you want to tolerate?',
          ('0', '1', '2', '3'))
     print("Maximum mutations in potential nGREs: ", maximum_mutations)
 
-    gene_sequence = st.text_area("Insert DNA sequence to parse for nGREs")
-    st.write(gene_sequence)
+    mismatch_penalty = st.selectbox(
+         'What do you want the mismatch penalty (number of points subtracted from a subsequence\'s score for every mismatch mutation) to be?',
+         ('1', '2', '3', '0'))
+
+    insertion_penalty = st.selectbox(
+         'What do you want the insertion penalty (number of points subtracted from a subsequence\'s score for every insertion mutation) to be?',
+         ('1', '2', '3', '0'))
+
+    deletion_penalty = st.selectbox(
+         'What do you want the deletion penalty (number of points subtracted from a subsequence\'s score for every deletion mutation) to be?',
+         ('1', '2', '3', '0'))
 
     search_string = "((?e)[Cc][Tt][Cc][Cc][TAGCtagc]?[TAGCtagc]?[TAGCtagc]?[Gg][Gg][Aa][Gg][Aa]){e<=" + maximum_mutations + "}"
     possible_nGREs = regex.findall(search_string, gene_sequence)
@@ -33,16 +45,12 @@ def nGRE_info_dataframe(possible_nGREs, gene_sequence):
 
         # find and extract start coordinate from pos_information using regex
         start_coordinate_regex = regex.findall(r"\(\d+,", str(pos_information))[0]
-        st.write("start_coordinate_regex: " + start_coordinate_regex)
         start_coordinate = int(regex.findall("\d+", str(start_coordinate_regex))[0])
-        st.write("start_coordinate: " + str(start_coordinate))
         nGRE_information["start"] = start_coordinate
 
         # find and extract end coordinate from pos_information using regex
         end_coordinate_regex = regex.findall(r" \d+\)", str(pos_information))[0]
-        st.write("end_coordinate_regex: " + end_coordinate_regex)
         end_coordinate = int(regex.findall("\d+", str(end_coordinate_regex))[0])
-        st.write("end_coordinate: " + str(end_coordinate))
         nGRE_information["end"] = end_coordinate
 
         comparison = (regex.search(r"((?e)[Cc][Tt][Cc][Cc][TAGCtagc]?[TAGCtagc]?[TAGCtagc]?[Gg][Gg][Aa][Gg][Aa]){e<=" + maximum_mutations + "}", nGRE_sequence))
