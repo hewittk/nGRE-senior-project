4dc0720cd6da1149e2d9e1279261b80812ebed58
Kiley Hewitt <42876297+hewittk@users.noreply.github.com>
2022-03-14 22:03:34 -0400

    Dataframe of sequence, start position, and end position

---
+++ nGRE_location_analyze.py
@@ -0,0 +1,4 @@
+# if looking at new gene
+# retrieve gene's chr, trx start site, coding start site, coding end site, and trx end site
+
+# retreive nGRE's chromosome and start site from nGRE parse output file
--- streamlit/nGRE_parse.py
+++ streamlit/nGRE_parse.py
@@ -16,3 +16,31 @@ def main():
     search_string = "((?e)[Cc][Tt][Cc][Cc][TAGCtagc]?[TAGCtagc]?[TAGCtagc]?[Gg][Gg][Aa][Gg][Aa]){e<=" + maximum_mutations + "}"
     possible_nGREs = regex.findall(search_string, gene_sequence)
     st.write(possible_nGREs)
+
+    nGRE_info_dataframe(possible_nGREs, gene_sequence)
+
+
+def nGRE_info_dataframe(possible_nGREs, gene_sequence):
+    nGRE_table = pd.DataFrame(columns = ["sequence", "start", "end"])
+
+    nGRE_information = {}
+    for nGRE_sequence in possible_nGREs:
+        nGRE_information["sequence"] = nGRE_sequence
+
+        pos_information = regex.search(nGRE_sequence, gene_sequence)
+
+        start_coordinate_regex = regex.findall(r"\(\d+,", str(pos_information))[0]
+        st.write("start_coordinate_regex: " + start_coordinate_regex)
+        start_coordinate = int(regex.findall("\d+", str(start_coordinate_regex))[0])
+        st.write("start_coordinate: " + str(start_coordinate))
+        nGRE_information["start"] = start_coordinate
+
+        end_coordinate_regex = regex.findall(r" \d+\)", str(pos_information))[0]
+        st.write("end_coordinate_regex: " + end_coordinate_regex)
+        end_coordinate = int(regex.findall("\d+", str(end_coordinate_regex))[0])
+        st.write("end_coordinate: " + str(end_coordinate))
+        nGRE_information["end"] = end_coordinate
+
+        nGRE_table = nGRE_table.append(nGRE_information, ignore_index = True)
+
+    st.dataframe(nGRE_table)
